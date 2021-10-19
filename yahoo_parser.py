96bbb30b003f01b5ecb13d7b5a76f268773976ad
backtofayton <yucelolcay@gmail.com>
2021-10-19 20:14:12 +0300

    'first'

--- yahoo_parser.py
+++ yahoo_parser.py
@@ -4,46 +4,50 @@ import re
 import json
 import time
 
+# Data format
+# Date,Open,High,Low,Close,Adj Close,Volume
+# 2020-07-13,97.264999,99.955002,95.257500,95.477501,94.696587,191649200
 
-def yahoo_parser(ticker):    # 347155200
+# 347155200
+
+
+def yahoo_parser(ticker):
     # Set time to 10 minutes ago to be safe
-    current_date = str(date.now().timestamp()-600)[:-7]
-    yahoo_link = 'https://query1.finance.yahoo.com/v7/finance/download/%s?period1=1594515200&period2=%s&interval=1d&events=history&includeAdjustedClose=true' % (
-        ticker, current_date)
+    current_date = str(date.now().timestamp() - 600)[:-7]
+    yahoo_link = (
+        "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=1594515200&period2=%s&interval=1d&events=history&includeAdjustedClose=true"
+        % (ticker, current_date)
+    )
     print(yahoo_link)
 
-    headers = {'user-agent': 'PostmanRuntime/7.28.4'}
+    headers = {"user-agent": "PostmanRuntime/7.28.4"}
 
     req = requests.get(yahoo_link, headers=headers)
     if req.status_code != 200:
         time.sleep(5)
+        # Return false to make start.py restart the function
         return False
     print(req)
 
-    # Data format
-    # Date,Open,High,Low,Close,Adj Close,Volume
-    # 2020-07-13,97.264999,99.955002,95.257500,95.477501,94.696587,191649200
-
-    with open('%s.json' % ticker, 'w') as fd:
+    with open("%s.json" % ticker, "w") as fd:
         # Regex format for date catch in such format: 2020-07-13
-        compileDate = re.compile(b'[0-9]{4}-[0-9]{2}-[0-9]{2}')
+        compileDate = re.compile(b"[0-9]{4}-[0-9]{2}-[0-9]{2}")
         titleParse = True
         chunkRemaining = 0
-        encoding = 'utf-8'
+        encoding = "utf-8"
 
         chunkObject = {}
         for chunk in req.iter_content(chunk_size=128):
+            # Regex catches all the data before '\n' at the start of the stream to set Titles
             if titleParse:
-                # Regex catches in the beginning of document for all the value before to set Titles '\n'
-                parsedTitles = re.compile(
-                    b'.*?(?=\n)').findall(chunk)[0].split(b',')
+                parsedTitles = re.compile(b".*?(?=\n)").findall(chunk)[0].split(b",")
                 print(parsedTitles)
                 titleParse = False
 
+            # Data from previous iteration which wasn't saved into dictionary
             if chunkRemaining:
-                chunk = chunkRemaining+chunk
+                chunk = chunkRemaining + chunk
 
-            print(chunk)
             # Check if there is complete Date data
             findDates = compileDate.findall(chunk)
 
@@ -52,28 +56,34 @@ def yahoo_parser(ticker):    # 347155200
                 # chunkObject = {}
                 for index in range(len(findDates)):
                     dateString = findDates[index]
-                    compileData = re.compile(
-                        b'(?<=%b,)(.*?)(?=\n)' % dateString)
+                    compileData = re.compile(b"(?<=%b,)(.*?)(?=\n)" % dateString)
                     findData = compileData.search(chunk)
                     # If regex catches data for given date, start parsing it
-                    print('index = ', index)
                     if findData:
-                        parsedData = chunk[findData.start(
-                        ):findData.end()].split(b',')
+                        parsedData = chunk[findData.start() : findData.end()].split(
+                            b","
+                        )
 
                         for titleIndex in range(len(parsedTitles)):
                             # First title is Date which is not inside parsed data, set it to date value
-                            if (titleIndex == 0):
+                            if titleIndex == 0:
                                 # Set key for given Date in Dictionary
                                 print(findDates[titleIndex])
-                                chunkObject[findDates[index].decode(encoding)] = {
-                                }
+                                chunkObject[findDates[index].decode(encoding)] = {}
                             else:
-                                # parsed Data doesnt have Date, set it's index to -1
+                                # parsed Data doesnt have Date field, so set it's index to -1
                                 chunkObject[findDates[index].decode(encoding)].update(
-                                    {parsedTitles[titleIndex].decode(encoding): parsedData[titleIndex-1].decode(encoding)})
+                                    {
+                                        parsedTitles[titleIndex]
+                                        .decode(encoding): parsedData[titleIndex - 1]
+                                        .decode(encoding)
+                                    }
+                                )
                         print(parsedData)
-                        chunkRemaining = chunk[findData.end():]
+
+                        # If there is complete Data set it will be appended to dictionary.
+                        # Save the remaining data into variable and prepend to string in the next iteration
+                        chunkRemaining = chunk[findData.end() :]
 
         json.dump(chunkObject, fd)
         print(len(chunkObject.keys()))
